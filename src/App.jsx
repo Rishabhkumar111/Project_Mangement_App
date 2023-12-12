adc36066c92391b8a8a87cc850c302fb23d3df90
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2023-12-12 17:49:20 +0530

    Collected User data using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,48 @@
+import { useState } from "react";
 import NewProject from "./Components/NewProject.jsx";
 import NoProjectSelected from "./Components/NoProjectSelected.jsx";
 import ProjectSidebar from "./Components/ProjectSidebar.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  })
+
+  function handleAddProject(data){
+    setProjectState(pre=>{
+      let newProject = {
+        ...data,
+        id: Math.random()
+      }
+      return {
+        ...pre,
+        projects : [...pre.projects, newProject]
+      }
+    })
+  }
+
+  function handleStartAddProject(){
+    setProjectState(pre=>{
+      return {
+        ...pre,
+        selectedProjectId: null
+      }
+    })
+  }
+  
+  let content;
+  
+  if(projectState.selectedProjectId === null){
+    content = <NewProject onAdd={handleAddProject}/>
+  }else if(projectState.selectedProjectId === undefined){
+    content = <NoProjectSelected onSelected={handleStartAddProject} />
+  }
+
   return (
     <main className=" h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onSelected={handleStartAddProject} />
+      {content}
     </main>
   );
 }
