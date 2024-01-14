85dc03c260bc8032594a39df2c50d82fa259a313
Rishabh Kumar <rishabhkumar@Rishabhs-MacBook-Pro.local>
2024-01-14 20:45:51 +0530

    Resolved deploying error

--- src/Components/NewProject.jsx
+++ src/Components/NewProject.jsx
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import Input from "./input.jsx";
+import Input from "./Input.jsx";
 import Modal from "./Modal.jsx";
 
 export default function NewProject({ onAdd , onCancle}) {
